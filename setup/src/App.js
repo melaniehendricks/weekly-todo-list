77705647cf65237e135ecc186588f1cf806c2bd0
Melanie Hendricks <melanie.r.hendricks@gmail.com>
2022-07-25 15:58:41 -0700

    added functionality: button to remove individual tasks once completed. updated CSS

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,15 +1,10 @@
-import React, { useState } from "react";
-import data from "./data";
+import React from "react";
 import List from "./List";
 function App() {
-  const [tasks, setTasks] = useState(data);
-
   return (
     <main>
       <section className="container">
-        <h3>{tasks.length} tasks this week</h3>
-        <List tasks={tasks} />
-        <button onClick={() => setTasks([])}>clear all</button>
+        <List />
       </section>
     </main>
   );
--- setup/src/List.js
+++ setup/src/List.js
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./data";
 
-const List = ({ tasks }) => {
+const List = () => {
+  const [tasks, setTasks] = useState(data);
+
+  const removeTask = (id) => {
+    let newTasks = tasks.filter((task) => task.id !== id);
+    setTasks(newTasks);
+  };
+
   return (
     <>
+      <h3>{tasks.length} tasks this week</h3>
       {tasks.map((task) => {
         const { id, name, time, image, source } = task;
         return (
-          <article key={id} className="person">
+          <article key={id} className="task">
             <a href={source}>
               <img src={image} alt={name} />
             </a>
             <div>
               <h4> {name}</h4>
               <p>{time}</p>
+              <button onClick={() => removeTask(id)}>Completed</button>
             </div>
           </article>
         );
       })}
+      <button className="clearAll" onClick={() => setTasks([])}>
+        All Completed
+      </button>
     </>
   );
 };
